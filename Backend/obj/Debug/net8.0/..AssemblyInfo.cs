f008067a81235e6c86911f429fc3ed40926756da
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2024-10-20 13:57:27 +0100

    "Fixed Commit issues"

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a493c4a0775b9a257db7dca69d631871e9b95cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f533ed0cbae94d92484594523aa05b385d84e8b")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
