7ec914133f59c8ded630a672a34cea8e4c82c6eb
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2024-12-01 23:25:59 +0000

    "Updated assembly version and source link in Backend project"

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febc64c27572de84fd463d2f78193dba15dc7eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de38f259a6c1f8519e4683ab209da21d3235ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
